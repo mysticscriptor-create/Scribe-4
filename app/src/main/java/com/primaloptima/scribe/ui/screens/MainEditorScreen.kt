9c22ddac738fd8c67c71503520ad25beee5ddad4
mysticscriptor-create <mysticscriptor@gmail.com>
2026-08-18 15:09:17 +0500

    update MainEditorScreen.kt

--- app/src/main/java/com/primaloptima/scribe/ui/screens/MainEditorScreen.kt
+++ app/src/main/java/com/primaloptima/scribe/ui/screens/MainEditorScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
